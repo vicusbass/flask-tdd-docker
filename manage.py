c7ebdcbf79d425e3991dbfa4386999fc08cc4b1c
Vasile Pop <vasile.pop@complyadvantage.com>
2020-02-09 20:22:17 +0200

    added Postgres, model for users

--- manage.py
+++ manage.py
@@ -1,8 +1,17 @@
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db
+
 
 cli = FlaskGroup(app)
 
-if __name__ == "__main__":
+
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+if __name__ == '__main__':
     cli()
